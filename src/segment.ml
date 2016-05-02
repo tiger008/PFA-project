9017b8f67eb3acf55f8752b94260cd20b2a1590d
Dimitrios Christaras-Papageorgiou <xritrios@xritrios.com>
2016-05-02 12:12:43 +0200

    creation de la fonction segment

--- src/segment.ml
+++ src/segment.ml
@@ -1,14 +1,28 @@
 type t = {id : string; 
           porig : Point.t; 
           pdest : Point.t;
+          ci : float;
+          ce : float;
          }
 
 type tpos = L | R | C
 
-let new_segment xo yo xd yd = failwith "TODO"
+let compteur i =
+  let etat = ref i in
+  fun () -> etat := !etat + 1; string_of_int !etat
+                      
+let get_id = compteur 0
 
-let get_position p s = failwith "TODO"
+let new_segment xo yo xd yd = { id = get_id (); porig = Point.new_point xo yo; pdest = Point.new_point xd yd; ci = 0.; ce = 100. }
 
-let split_segment d s = failwith "TODO"
+let get_position p s =
+  let open Point in
+  let z = (s.pdest.x - s.porig.x) * (p.y - s.porig.y) - (s.pdest.y - s.porig.y) * (p.x - s.porig.x) in
+  match z with
+  | 0 -> C
+  | _ when z < 0 -> R
+  | _ -> L
+           
+let split_segment s1 s2 = 
 
 let split hd rest = failwith "TODO"

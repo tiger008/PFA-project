fa12d45503cc7bf0cedb07fedf0fd9e4d1aae014
Dimitrios Christaras-Papageorgiou <xritrios@xritrios.com>
2016-05-03 10:33:30 +0200

    creation du fichier physic.ml

--- src/physic.ml
+++ src/physic.ml
@@ -1,1 +1,11 @@
-let detect_collision p bsp = failwith "TODO"
+let detect_collision p bsp =
+  let rec rdc bsp =
+    match bsp with
+    | E -> false
+    | N(r,ag, ad) ->
+       let c = get_position p r in
+       match c with
+       | C -> true
+       | L -> rdc ag
+       | R -> rdc ad
+  in rdc bsp

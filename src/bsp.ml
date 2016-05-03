679bc6491c5538238c09db59163176754dc1f337
Dimitrios Christaras-Papageorgiou <xritrios@xritrios.com>
2016-05-03 10:18:22 +0200

    creation du fichier bsp.ml

--- src/bsp.ml
+++ src/bsp.ml
@@ -1,11 +1,43 @@
 open Segment
+open Point
 
 type t = E | N of Segment.t * t * t 
+                                    
+let parse f bsp p =
+  let rec rparse bsp =
+    match bsp with
+    | E -> ()
+    | N(r,sl,sr) ->
+       let pos = get_position p r in
+       match pos with
+       | C -> f r; ()
+       | L -> rparse sl; f r; rparse sr 
+       | R -> rparse sr; f r; rparse sl
+  in rparse bsp
 
-let parse f bsp p = failwith "TODO"
+let rev_parse f bsp p =
+  let rec rrevparse bsp =
+    match bsp with
+    | E -> ()
+    | N(r,sl,sr) ->
+       let pos = get_position p r in
+       match pos with
+       | C -> f r; ()
+       | L -> rrevparse sr; f r; rrevparse sl 
+       | R -> rrevparse sl; f r; rrevparse sr
+  in rrevparse bsp
 
-let rev_parse f bsp p = failwith "TODO"
+let iter f bsp =
+  let rec riter bsp =
+    match bsp with
+    | E -> ()
+    | N(r,sl,sr) -> f r; riter sl; riter sr
+  in riter bsp
 
-let iter f bsp = failwith "TODO"
-
-let build_bsp sl = failwith "TODO"
+let build_bsp sl =
+  let rec rbuild = function
+    | [] -> E
+    | x::s -> let (sll, slr) = split x s in
+              N(x, rbuild sll, rbuild slr)
+  in rbuild sl
+    

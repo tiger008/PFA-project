287b8982196861f67c4084158d381c754fb7dc88
Thomas Perraut <tperrau@a-018733.ups.u-psud.fr>
2016-05-03 09:19:33 +0200

    player modified

--- src/player.ml
+++ src/player.ml
@@ -1,9 +1,9 @@
 open Options
 open Physic
 open Point
-       
+
 type t = {
-  mutable pos : Point.t;
+    mutable pos : Point.t;
   mutable pa : int;
 }
 
@@ -12,20 +12,24 @@ let new_player pos pa = { pos; pa }
 type dir = Left | Right
 
 let rotate d p =
-  match d with
+    match d with
   | Left -> p.pa <- (p.pa + 1) mod 360
   | Right -> p.pa <- (p.pa - 1) mod 360
 
 type mv = MFwd | MBwd | MLeft | MRight
 
+let iof n = int_of_float n
+
+let foi n = float_of_int n
+
 let move d p bsp =
-  let dx, dy =
-  match d with
-  | MFwd -> 0., -1.
-  | MBwd -> 0., 1.
-  | MLeft -> -1., 0.
-  | MRight -> 1., 0. in
-  let np = Point.new_point (int_of_float (((float_of_int p.pos.x) +. dx) *. cos (float_of_int p.pa))) (int_of_float (((float_of_int p.pos.y) +. dy) *. sin (float_of_int p.pa))) in
-  if not (detect_collision np bsp) then
-    p.pos <- np
-  else ()
+    let dx, dy =
+        match d with
+        | MFwd -> 0., -1.
+        | MBwd -> 0., 1.
+        | MLeft -> -1., 0.
+        | MRight -> 1., 0. in
+    let np = Point.new_point (iof (((foi p.pos.x) +. dx) *. cos (foi p.pa))) (iof (((foi p.pos.y) +. dy) *. sin (foi p.pa))) in
+    if not (detect_collision np bsp) then
+        p.pos <- np
+    else ()

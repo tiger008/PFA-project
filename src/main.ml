c78ee2668f43a05758d8612d031e9a32ec5b54ff
Dimitrios Christaras-Papageorgiou <xritrios@xritrios.com>
2016-05-06 11:34:08 +0200

    mise en place de display

--- src/main.ml
+++ src/main.ml
@@ -7,6 +7,7 @@ open Point
 open Bsp
 open Trigo
 open Sys
+open Render
 
 let rec seglist il acc =
     match il with
@@ -14,37 +15,39 @@ let rec seglist il acc =
   | (xo, yo, xd, yd)::s -> seglist s ((new_segment xo yo xd yd)::acc)
 
 let () =
-    let ((x, y, a), sl) = read_lab (open_in argv.(1))  in
-    let player = new_player (new_point x y) a in
-    let map = build_bsp (seglist sl []) in
-    open_graph (Printf.sprintf " %dx%d" win_w win_h);
+  let ((x, y, a), sl) = read_lab (open_in argv.(1))  in
+  let player = new_player (new_point x y) a in
+  let map = build_bsp (seglist sl []) in
+  open_graph (Printf.sprintf " %dx%d" win_w win_h);
   auto_synchronize false;
   try
-          let angle1 = [(player.pos.x, player.pos.y, player.pos.x + (int_of_float (10. *. (dcos (player.pa-30)))), player.pos.y + (int_of_float (20. *. (dsin (player.pa-30)))))] in
+    (*let angle1 = [(player.pos.x, player.pos.y, player.pos.x + (int_of_float (10. *. (dcos (player.pa-30)))), player.pos.y + (int_of_float (20. *. (dsin (player.pa-30)))))] in
           let angle2 = [(player.pos.x, player.pos.y, player.pos.x + (int_of_float (10. *. (dcos (player.pa+30)))), player.pos.y + (int_of_float (20. *. (dsin (player.pa+30)))))] in
       draw_segments (Array.of_list sl);
       draw_segments (Array.of_list angle1);
-      draw_segments (Array.of_list angle2);
-      synchronize ();
-      while true do
-          let s = Graphics.wait_next_event [Graphics.Key_pressed] in
-          let _ = match s.key with
-          | 'w' | 'z' -> move MFwd player map
-          | 's' -> move MBwd player map
-          | 'a' | 'q'-> move MLeft player map
-          | 'd' -> move MRight player map
-          | 'e' -> rotate Left player
-          | 'r' -> rotate Right player
-          | '\027' -> raise Exit
-          | _ -> ()
-          in
-          (*display map player;*)
-          clear_graph ();
-          let angle1 = [(player.pos.x, player.pos.y, player.pos.x + (int_of_float (10. *. (dcos (player.pa-30)))), player.pos.y + (int_of_float (20. *. (dsin (player.pa-30)))))] in
-          let angle2 = [(player.pos.x, player.pos.y, player.pos.x + (int_of_float (10. *. (dcos (player.pa+30)))), player.pos.y + (int_of_float (20. *. (dsin (player.pa+30)))))] in
+      draw_segments (Array.of_list angle2);*)
+    display map player;
+    synchronize ();
+    while true do
+      let s = Graphics.wait_next_event [Graphics.Key_pressed] in
+      let _ = match s.key with
+        | 'w' | 'z' -> move MFwd player map
+        | 's' -> move MBwd player map
+        | 'a' | 'q'-> move MLeft player map
+        | 'd' -> move MRight player map
+        | 'e' -> rotate Left player
+        | 'r' -> rotate Right player
+        | '\027' -> raise Exit
+        | _ -> ()
+      in
+      (*display map player;*)
+      clear_graph ();
+      (*let angle1 = [(player.pos.x, player.pos.y, player.pos.x + (int_of_float (10. *. (dcos (player.pa-30)))), player.pos.y + (int_of_float (20. *. (dsin (player.pa-30)))))] in
+          let angle2 = [(player.pos.x, player.pos.y, player.pos.x + (int_of_float (10. *. (dcos (9player.pa+30)))), player.pos.y + (int_of_float (20. *. (dsin (player.pa+30)))))] in
           draw_segments (Array.of_list sl);
           draw_segments (Array.of_list angle1);
-          draw_segments (Array.of_list angle2);
-          synchronize ();
-      done;
-          with Exit -> close_graph (); exit (0)
+          draw_segments (Array.of_list angle2);*)
+      display map player;
+      synchronize ();
+    done;
+  with Exit -> close_graph (); exit (0)

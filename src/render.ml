8dadc1df28baf22cd4245f6317f473db270a8ceb
Dimitrios Christaras-Papageorgiou <xritrios@xritrios.com>
2016-05-09 22:53:21 +0200

    quelques modifs pour clipping

--- src/render.ml
+++ src/render.ml
@@ -5,36 +5,53 @@ open Trigo
 open Graphics
 open Bsp
        
-let tr s p =
+(*let translation_rotation s p =
   {s with
     porig = new_point (iof ((foi (s.porig.x - p.pos.x)) *. (dcos (-p.pa)) -. (foi (s.porig.y - p.pos.y)) *. (dsin (-p.pa)))) (iof ((foi (s.porig.y - p.pos.y)) *. (dcos (-p.pa)) +. (foi (s.porig.x - p.pos.x)) *. (dsin (-p.pa))));
     pdest = new_point (iof ((foi (s.pdest.x - p.pos.x)) *. (dcos (-p.pa)) -. (foi (s.pdest.y - p.pos.y)) *. (dsin (-p.pa)))) (iof ((foi (s.pdest.y - p.pos.y)) *. (dcos (-p.pa)) +. (foi (s.pdest.x - p.pos.x)) *. (dsin (-p.pa))))
-  }
-    
-let itr s p =
+  }*)
+
+let vrai_point1 s =
+  new_point (iof ((foi s.porig.x) +. s.ci *. (foi s.lx))) (iof ((foi s.porig.y) +. s.ci *. (foi s.ly)))
+
+let vrai_point2 s =
+  new_point (iof ((foi s.pdest.x) +. s.ce *. (foi s.lx))) (iof ((foi s.pdest.y) +. s.ce *. (foi s.ly)))
+       
+let translation_rotation s p =
   {s with
-    porig = translation (rotation s.porig p.pa) (new_point p.pos.x p.pos.y);
-    pdest = translation (rotation s.pdest p.pa) (new_point p.pos.x p.pos.y)
+    porig = rotation (translation (vrai_point1 s) (new_point (-p.pos.x) (-p.pos.y))) (-p.pa);
+    pdest = rotation (translation (vrai_point2 s) (new_point (-p.pos.x) (-p.pos.y))) (-p.pa);
+  }
+            
+let translation_rotation_inverse s p =
+  {s with
+    porig = translation (rotation (vrai_point1 s) p.pa) (new_point (-p.pos.x) (-p.pos.y));
+    pdest = translation (rotation (vrai_point2 s) p.pa) (new_point (-p.pos.x) (-p.pos.y))
   }
 
 let angle s =
-    atan (foi (s.pdest.y - s.porig.y)) /. (foi (s.pdest.x - s.porig.x))
+    atan2 (((foi s.pdest.y) +. s.ce *. (foi s.ly)) -. ((foi s.porig.y) +. s.ci *. (foi s.ly))) (((foi s.pdest.x) +. s.ce *. (foi s.lx)) -. ((foi s.porig.x) +. s.ci *. (foi s.lx)))
 
-let clip bsp p =
+let clip l p =
   let rec rclip acc = function
-    | E -> acc
-    | N(r, ag, ad) -> let r = get_segment r in let a = tr r p in
-      if a.porig.x < 1 && a.pdest.x < 1 then
-        List.rev_append (rclip acc ag) (rclip acc ad)
-      else if a.porig.x < 1 then
-        let seg = {a with porig = new_point 1 (iof (foi (a.porig.y + (1 - a.porig.x)) *. tan ((angle a)))); pdest = new_point a.pdest.x a.pdest.y} in
-        List.rev_append (rclip ((itr seg p)::acc) ag) (rclip acc ad)
-      else if a.porig.y < 1 then
-        let seg = {a with porig = new_point a.porig.x a.porig.y; pdest = new_point 1 (iof (foi (a.pdest.y + (1 - a.pdest.x)) *. tan ((angle a))))} in
-        List.rev_append (rclip ((itr seg p)::acc) ag) (rclip acc ad)
-      else List.rev_append (rclip (r::acc) ag) (rclip acc ad)
-  in rclip [] bsp
-  
+    | [] -> acc
+    | r::s -> let a = translation_rotation r p in
+              if a.porig.x < 1 && a.pdest.x < 1 then
+                rclip acc s
+              else if a.porig.x < 1 then
+                let seg = {a with porig = new_point 1 (iof (foi (a.porig.y + (1 - a.porig.x)) *. tan ((angle a)))); pdest = new_point a.pdest.x a.pdest.y} in
+                rclip ((translation_rotation_inverse seg p)::acc) s
+              else if a.porig.y < 1 then
+                let seg = {a with porig = new_point a.porig.x a.porig.y; pdest = new_point 1 (iof (foi (a.pdest.y + (1 - a.pdest.x)) *. tan ((angle a))))} in
+                rclip ((translation_rotation_inverse seg p)::acc) s
+              else rclip (r::acc) s
+  in rclip [] l
+           
+let rec bsp_to_list = function
+  | E -> []
+  | N(r,ag,ad) ->
+     List.rev_append [r] (List.rev_append (bsp_to_list ag) (bsp_to_list ad))
+         
 (*let rec array_segments bsp acc =
   match bsp with
   | E -> acc
@@ -54,11 +71,7 @@ let display bsp player =
   let angle2 = [(player.pos.x, player.pos.y, player.pos.x + (iof (10. *. (dcos (player.pa+30)))), player.pos.y + (iof (20. *. (dsin (player.pa+30)))))] in
   draw_segments (Array.of_list angle1);
   draw_segments (Array.of_list angle2);
-  let map = clip bsp player in
-  (*let rec recd = function
-    | E -> ()
-    | N(r,ag,ad) -> r; recd ag; recd ad
-    in recd map*)
+  let map = clip (bsp_to_list bsp) player in
   draw map
 
                                 
